dfb197f3fcc91d0d52a690bceba70fb437fbcca4
Tina Yang <Tinayangers@gmail.com>
2023-03-16 12:27:04 -0400

    Added methods to classlist and created add class page

--- grade-me/src/pages/ClassList.vue
+++ grade-me/src/pages/ClassList.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>Home</div>
+    <div class="classListContainer">
+        <h1 class="classTitle">Classes</h1>
+        <router-link class="addClassButton" to="/addclass">Add Class</router-link>
+        <!-- <div class="classListResults" :key="class.id" v-for="class in classes"> -->
+        <!-- <h1 class="classTitle">{{class.name}}</h1> -->
+        <!-- </div> -->
+    </div>
 </template>
 <script>
+import axios from 'axios'
+
+export default {
+    name: 'ClassList',
+    components: {},
+    data: () => ({
+        classes: [],
+    }),
+    mounted() {
+        this.getClasses()
+    },
+    methods: {
+        async getClasses() {
+            const res = await axios.get(`http://localhost:3001/api/`)
+            this.classes = res.data
+            console.log(res)
+        }
+    }
+}
 </script>
