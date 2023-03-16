aae41674d1ddf16f5a475ce8c672d15778d8179c
Tina Yang <Tinayangers@gmail.com>
2023-03-16 11:10:51 -0400

    Updated routes and imported dependencies

--- grade-me/src/router.js
+++ grade-me/src/router.js
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage'
 import AboutPage from './components/AboutPage'
-import ClassPage from './components/ClassPage'
-import StudentPage from './components/StudentPage'
+import ClassList from './components/ClassList'
+import StudentList from './components/StudentList'
 
 const routes = [
     { path: '/', component: HomePage, name: 'HomePage' },
     { path: '/about', component: AboutPage, name: 'AboutPage' },
-    { path: '/class', component: ClassPage, name: 'ClassPage' },
-    { path: '/student', component: StudentPage, name: 'StudentPage' },
+    { path: '/classlist', component: ClassList, name: 'ClassList' },
+    { path: '/classlist/:id', component: ClassPage, name: 'ClassPage' },
+    { path: '/classlist/:id/studentlist', component: StudentList, name: 'StudentList' },
+    { path: '/student/:id', component: StudentPage, name: 'StudentPage' },
 ]
 
 const router = createRouter({
