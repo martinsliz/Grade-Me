cee32ceb26f22414df2a144089494d40b4ff7109
Liz Martins <elizabeth.martins5@gmail.com>
2023-03-16 14:11:44 -0400

    student form

--- grade-me/src/components/NewStudent.vue
+++ grade-me/src/components/NewStudent.vue
@@ -1,13 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <form>
+      <input/>
+    </form>
+  </div>
+
 </template>
 
 <script>
   export default {
-    name: 'NavBar'
+    name: 'NewStudent'
   }
 </script>
 
