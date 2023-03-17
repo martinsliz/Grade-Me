2f89b9a6f89bcd5d99cbb425626ac4d00140555f
Tina Yang <Tinayangers@gmail.com>
2023-03-17 10:23:48 -0400

    added update grade - non functional

--- grade-me/src/components/ViewGrade.vue
+++ grade-me/src/components/ViewGrade.vue
@@ -3,16 +3,25 @@
         <h1>Current Grade</h1>
         <h3>Student Name: {{ name }}</h3>
         <h3>Grade For This Class: {{ grade }}</h3>
+        <p>Update Grade</p>
+        <form @submit="changeGrade">
+            <input :value="inputValue" placeholder="Enter Grade" @click="handleChange">
+            <button type="submit">Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+
 export default {
     name: 'ViewGrade',
     data: () => ({
         grade: '',
-        name: ''
+        name: '',
+        updateGrade: '',
+        newGrade: ''
     }),
     mounted() {
         this.getStudentsGradeByClass()
@@ -24,6 +33,16 @@ export default {
             console.log(res)
             this.name = res.data.studentInfo.name
         },
+        async changeGrade(e) {
+            e.preventDefault()
+            const res = await axios.put(`http://localhost:3001/api/classlists/${this.$route.params.classId}/class/${this.$route.params.studentId}`, { grade: this.newGrade })
+            // this.updateGrade = res.data
+            console.log(res)
+            this.getStudentsGradeByClass()
+        },
+        handleChange(event) {
+            this.newGrade = event.target.value
+        }
 
     }
 }
